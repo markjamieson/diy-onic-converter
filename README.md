f1aa6c32594a9bf649df7f9a105e28af53f26845
Mark <mark.jamieson@snyk.io>
2024-10-04 16:21:21 -0300

    feat: allow arbitrary CSS rules on bolded and remaining parts of words

--- README.md
+++ README.md
@@ -5,14 +5,30 @@ A converter function which implements a simplified version of bionic reading on 
 ## Usage
 
 1. Copy/paste the full contents of [`public/diy-onic-converter.js`](public/diy-onic-converter.js) into the browser console.
-2. Call the function on a CSS selector using `diyOnicConverter(selector, charsToBold?)`. e.g.
+2. Call the function on a CSS selector using `diyOnicConverter(selector, options?)`. e.g.
 ```JavaScript
 // Converts full body with default number of characters bolded
 diyOnicConverter('body');
 
 // Converts first instance of article element with class `long-read`, only bolding the first letter of each word
-diyOnicConverted('article.long-read', 1);
+diyOnicConverter('article.long-read', { numChars: 1 });
+
+// Converts full body with first 2 characters extra-bolded and underlined, and remaining characters slightly lighter in color
+diyOnicConverter('body', {
+  numChars: 2, 
+  boldCssRules: ['font-weight: bolder', 'text-decoration: underline'], 
+  remainderCssRules: ['color: #0A0A0A'],
+});
 ```
+
+`options` argument accepts the following:
+
+| param | type | description |
+|--------|------|-------------|
+|`numChars`|number|number of characters at the start of each word to bold|
+|`boldCssRules`|string[]|array of CSS rules to apply to the bolded section of each word|
+|`remainderCssRules`|string[]|array of CSS rules to apply to the non-bolded section of each word|
+
 
 This function should work on any webpage which wraps content in `<p>` tags. Tested on Wikipedia and The Guardian and both worked as expected.
 
@@ -30,3 +46,7 @@ Finally, a stylesheet is appended to the document (if it hasn't already been app
 ## Caveats
 
 - All white-space characters in the converted paragraphs will be converted to simple space characters. This means that any non-space whitespace characters will be modified.
+
+## Code notes
+
+- The helper functions are all nested inside the main function to satisfy the requirements of the challenge. Stylistically I would prefer them outside the main function for readability.
--- public/diy-onic-converter.js
+++ public/diy-onic-converter.js
@@ -1,14 +1,51 @@
 /**
  * Implement your converter function here.
  */
-const diyOnicConverter = (textContentContainerSelector, charsToBold = 3) => {
+const diyOnicConverter = (
+  textContentContainerSelector,
+  opts = { numChars: 3, boldCssRules: undefined, remainderCssRules: undefined }
+) => {
+  const validateOptions = (opts) => {
+    if (typeof opts !== 'object') {
+      console.warn('Invalid `opts` argument, reverting to default value');
+      opts = {
+        numChars: 3,
+        boldCssRules: undefined,
+        remainderCssRules: undefined,
+      };
+    }
+    const validatedOptions = { ...opts };
+    if (typeof opts.numChars !== 'number' || opts.numChars < 1) {
+      console.warn(
+        'Invalid `charsToBold` argument, reverting to default value'
+      );
+      validatedOptions.numChars = 3;
+    }
+    if (opts.boldCssRules && !Array.isArray(opts.boldCssRules)) {
+      console.warn('Invalid `boldCssRules` argument, reverting to undefined');
+      validatedOptions.boldCssRules = undefined;
+    }
+    if (opts.remainderCssRules && !Array.isArray(opts.remainderCssRules)) {
+      console.warn(
+        'Invalid `remainderCssRules` argument, reverting to undefined'
+      );
+      validatedOptions.remainderCssRules = undefined;
+    }
+    return validatedOptions;
+  };
+
   const convertWord = (word) => {
-    const toBold = word.substr(0, charsToBold);
-    const remainder = word.substr(charsToBold);
-    const span = document.createElement('span');
-    span.className = 'diy-onic-converter__bold';
-    span.innerText = toBold;
-    return `${span.outerHTML}${remainder}`;
+    const toBold = word.substr(0, numChars);
+    const remainder = word.substr(numChars);
+
+    const boldSpan = document.createElement('span');
+    boldSpan.className = 'diy-onic-converter__bold';
+    boldSpan.innerText = toBold;
+
+    const remainderSpan = document.createElement('span');
+    remainderSpan.className = 'diy-onic-converter__remainder';
+    remainderSpan.innerText = remainder;
+    return `${boldSpan.outerHTML}${remainderSpan.outerHTML}`;
   };
 
   const convertParagraph = (paragraph) => {
@@ -18,9 +55,20 @@ const diyOnicConverter = (textContentContainerSelector, charsToBold = 3) => {
     return convertedWords.join(' ');
   };
 
+  const insertStyleRule = (styleSheet, rule, subSelector) => {
+    if (typeof rule !== 'string') return;
+    if (!rule.endsWith(';')) rule = `${rule};`;
+    styleSheet?.insertRule(`
+          .diy-onic-converter__${subSelector} {
+            ${rule}
+          }`);
+  };
+
   const insertStyleSheet = () => {
     const ID = 'diy-onic-converter-stylesheet';
-    if (document.getElementById(ID)) return;
+    if (document.getElementById(ID)) {
+      document.getElementById(ID)?.remove();
+    };
     const styleSheet = document.createElement('style');
     styleSheet.id = ID;
     document.head.append(styleSheet);
@@ -29,19 +77,29 @@ const diyOnicConverter = (textContentContainerSelector, charsToBold = 3) => {
         font-weight: 700;
       }
     `);
+    if (boldCssRules) {
+      boldCssRules.forEach((rule) =>
+        insertStyleRule(styleSheet.sheet, rule, 'bold')
+      );
+    }
+    if (remainderCssRules) {
+      remainderCssRules.forEach((rule) =>
+        insertStyleRule(styleSheet.sheet, rule, 'remainder')
+      );
+    }
   };
 
   const container = document.querySelector(textContentContainerSelector);
   console.log('Performing bionic reading conversion on:', container);
 
-  if (!container) {
-    console.error('Failed to find a container with given selector', textContentContainerSelector);
-    return;
-  }
+  const { numChars, boldCssRules, remainderCssRules } = validateOptions(opts);
 
-  if (typeof charsToBold !== 'number' || charsToBold <= 0) {
-    console.warn('Invalid `charsToBold` argument, reverting to default value')
-    charsToBold = 3;
+  if (!container) {
+    console.error(
+      'Failed to find a container with given selector',
+      textContentContainerSelector
+    );
+    return;
   }
 
   const paragraphs = container.querySelectorAll('p');
